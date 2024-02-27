9148d41a170c5f1e4a7c7322248169caac7fbe0b
Abdul wasay khan <156195187+abdulwasayk14@users.noreply.github.com>
2024-02-27 10:24:21 +0500

    Add files via upload

---
+++ Alien_colors_3.js
@@ -0,0 +1,34 @@
+"use strict";
+//Version 1 (Green Alien - 5 points):
+let alien_color1 = 'green';
+if (alien_color1 === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else if (alien_color1 === 'yellow') {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color1 === 'red') {
+    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+}
+//Version 2 (Yellow Alien - 10 points):
+let alien_color2 = 'yellow';
+if (alien_color2 === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else if (alien_color2 === 'yellow') {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color2 === 'red') {
+    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+}
+//Version 3 (Red Alien - 15 points):
+let alien_color3 = 'red';
+if (alien_color3 === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else if (alien_color3 === 'yellow') {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color3 === 'red') {
+    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+}
